1aff107739729c49838d6206b5d27a147e81d7f1
문정환 <70272622+bazzi2548@users.noreply.github.com>
2025-11-25 23:12:42 +0900

    model 변경

--- src/main/java/org/example/mcppractice/service/GPTService.java
+++ src/main/java/org/example/mcppractice/service/GPTService.java
@@ -1,6 +1,12 @@
 package org.example.mcppractice.service;
 
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -11,36 +17,38 @@ import okhttp3.Response;
 @Service
 public class GPTService {
 
-	private final OkHttpClient client = new OkHttpClient();
+	private final OkHttpClient client = new OkHttpClient.Builder()
+		.connectTimeout(20, TimeUnit.SECONDS)
+		.writeTimeout(20, TimeUnit.SECONDS)
+		.readTimeout(60, TimeUnit.SECONDS) // 가장 중요!
+		.build();;
+	private final ObjectMapper mapper = new ObjectMapper();
 	private static final String URL = "https://api.openai.com/v1/responses";
-	private final String apiKey = System.getenv("API_KEY");
+
+	@Value("${api.key}")
+	private String apiKey;
+
 
 	public String reviewCode(String code) throws Exception {
+		Map<String, Object> payload = Map.of(
+			"model", "gpt-5-nano",
+			"input", "너는 코드리뷰 전문가야. 아래 코드를 리뷰해줘:\n\n" + code
+		);
 
-		String prompt = """
-            너는 코드리뷰 전문가야.
-            코드리뷰 후 문제점/개선점을 알려줘.
-            
-            코드:
-            """ + code;
+		String json = mapper.writeValueAsString(payload);
 
-		String json = """
-        {
-          "model": "gpt-5.1-mini",
-          "input": "%s"
-        }
-        """.formatted(prompt.replace("\"","\\\""));
+		RequestBody body = RequestBody.create(
+			json,
+			MediaType.get("application/json")
+		);
 
-		RequestBody body = RequestBody.create(json, MediaType.get("application/json"));
 		Request request = new Request.Builder()
-			.url(URL)
+			.url("https://api.openai.com/v1/responses")
 			.header("Authorization", "Bearer " + apiKey)
 			.post(body)
 			.build();
 
 		Response response = client.newCall(request).execute();
-
 		return response.body().string();
 	}
-
 }
