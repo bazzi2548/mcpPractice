3d0dcb39fe87292c990667d00a291a2158da1b39
문정환 <70272622+bazzi2548@users.noreply.github.com>
2025-11-25 22:26:41 +0900

    Initial commit

---
+++ src/main/java/org/example/mcppractice/McpPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.mcppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mcppractice/McpPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mcppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
